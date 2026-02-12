f58ac28cc9a17a84fa974fcf31a84b87247afef7
youssefshoukry1 <youssefshoukry1@hotmail.com>
2026-02-12 20:35:42 +0200

    update project

--- Utility/paymob.js
+++ Utility/paymob.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
-// Paymob API Base URL (Egypt)
-const PAYMOB_API_URL = 'https://egypt.paymob.com/api';
+// Paymob API Base URL (Standard)
+const PAYMOB_API_URL = 'https://accept.paymob.com/api';
 
 /**
  * Paymob Payment Gateway Service
